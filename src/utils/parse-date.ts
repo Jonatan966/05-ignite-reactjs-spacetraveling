01ede8c5fef3e2bf376049bb547bab610240c07b
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-12-16 00:22:32 -0300

    fix: correct parse date

--- src/utils/parse-date.ts
+++ src/utils/parse-date.ts
@@ -2,7 +2,7 @@ import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
 export function parseDate(target: string): string {
-  return format(new Date(target), 'i LLL yyyy', {
+  return format(new Date(target), 'dd MMM yyyy', {
     locale: ptBR,
   });
 }
